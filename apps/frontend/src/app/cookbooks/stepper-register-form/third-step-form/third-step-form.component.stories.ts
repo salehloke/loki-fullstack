ae16c68b25731627fe598bad1d8c9fd471ae6417
salehloke <salehloke@gmail.com>
2023-08-13 21:27:23 +0800

    e2e testing wip

--- apps/frontend/src/app/cookbooks/stepper-register-form/third-step-form/third-step-form.component.stories.ts
+++ apps/frontend/src/app/cookbooks/stepper-register-form/third-step-form/third-step-form.component.stories.ts
@@ -11,7 +11,7 @@ const meta: Meta<ThirdStepFormComponent> = {
   decorators: [
     moduleMetadata({
       declarations: [],
-      imports: [ThirdStepFormComponent, MatInputModule],
+      imports: [MatInputModule],
     }),
   ],
 };
